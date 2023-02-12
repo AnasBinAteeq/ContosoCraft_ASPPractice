97877e75150b9ee96c1bb9970088af6083a308b7
Anas <anasbinateeq15@gmail.com>
2023-02-12 15:17:34 +0500

    First Commit

---
+++ Controllers/ProductsController.cs
@@ -0,0 +1,35 @@
+﻿using ASPtutorial.Models;
+using ASPtutorial.Services;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ASPtutorial.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        public ProductsController(JsonFileProductService productService)
+        {
+            this.ProductService= productService;
+        }
+
+        public JsonFileProductService ProductService { get;}
+
+        [HttpGet]
+        public IEnumerable<Product> Get()
+        {
+            return ProductService.GetProducts();
+        }
+
+        [HttpGet]
+        [Route("rate")]
+        public ActionResult Get(
+            [FromQuery] string ProductId,
+            [FromQuery] int Rating)
+        {
+            ProductService.AddRating(ProductId, Rating);
+            return Ok();
+        }
+    }
+}
